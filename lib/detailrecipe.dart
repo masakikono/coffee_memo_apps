41f7d2ca6399dbea1cbdf51636dfd7cee46b5bde
Masaki Kono <masaki19931015@gmail.com>
2022-03-04 10:46:52 +0900

    add:Screen transition implementation

---
+++ lib/detailrecipe.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailRecipe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('レシピ詳細'),
+      ),
+      body: Container(
+        color: Colors.red,
+
+      ),
+    );
+  }
+}
---
+++ lib/modifyrecipe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ModifyRecipe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('レシピ修正'),
+      ),
+      body: Container(
+        color: Colors.green,
+      ),
+    );
+  }
+}
